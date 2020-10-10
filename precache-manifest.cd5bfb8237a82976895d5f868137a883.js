d8869a6036cbeb3d5d297cee0e7233722a57c4ab
Murad <murad-arslangiraev-1994@mail.ru>
2020-10-10 13:44:16 +0300

    Updates

---
+++ precache-manifest.cd5bfb8237a82976895d5f868137a883.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6442e22fe4691fe31c67b3bda46cb888",
+    "url": "/cryptoApp/index.html"
+  },
+  {
+    "revision": "437dc3eae52d3d82b08e",
+    "url": "/cryptoApp/static/js/2.657e2cca.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/cryptoApp/static/js/2.657e2cca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ce76584255ff1aa800fb",
+    "url": "/cryptoApp/static/js/main.e105bb64.chunk.js"
+  },
+  {
+    "revision": "fdb4b6beab98c4ac15d7",
+    "url": "/cryptoApp/static/js/runtime-main.c236f677.js"
+  }
+]);
